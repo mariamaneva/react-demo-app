1280ba376ca58917addf76b50ca2bd3f8b3174c0
Maria Maneva <maria.maneva@goconsensus.com>
2024-09-01 00:19:41 +0300

    update prod server port

--- src/server/server-prod.js
+++ src/server/server-prod.js
@@ -4,7 +4,7 @@ import express from 'express'
 const app = express()
 const DIST_DIR = __dirname
 const HTML_FILE = path.join(DIST_DIR, 'index.html')
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 3000
 
 app.use(express.static(DIST_DIR))
 
